90283b747e52c562549e138096252560482ccfcd
Sam-Chug <sfackler96@gmail.com>
2022-09-22 02:31:33 -0400

    Updated version number

--- CSus2Editor/form/mainWindow.cs
+++ CSus2Editor/form/mainWindow.cs
@@ -16,7 +16,7 @@ namespace CSus2Editor
     public partial class mainWindow : Form
     {
         //So I dont forget to change it in the texts again lmao
-        public static string version = "v1.3.0";
+        public static string version = "v1.3.1";
 
         //Note column list
         public static List<NoteColumn> noteCols = new List<NoteColumn>();
